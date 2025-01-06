21b9b4e3cd86eefe63379345e8b114057920651b
juandejunin <juandejunin75@gmail.com>
2025-01-06 11:14:35 +0100

    Refactor: separate app initialization from index to enhance modularity and enable easier testing

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import path from 'path';
+import userRoutes from './routes/user.routes';
+
+const app = express();
+
+// Middleware global
+app.use(express.json());
+
+// Rutas
+app.use('/api/users', userRoutes);
+
+// Archivos estáticos
+app.use(express.static(path.join(__dirname, '../public')));
+
+export default app;
